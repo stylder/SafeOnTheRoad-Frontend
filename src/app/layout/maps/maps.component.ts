2e01e38638eb5ffa13e95c42fdf066696e8fcb1c
Santiago García Cabral <stylder@gmail.com>
2017-08-31 12:03:18 -0500

    Se realizó el ejemplo de Google Maps

--- src/app/layout/maps/maps.component.ts
+++ src/app/layout/maps/maps.component.ts
@@ -8,14 +8,66 @@ import {routerTransition} from '../../router.animations';
     styleUrls: ['./maps.component.scss'],
     animations: [routerTransition()]
 })
+
+
+
 export class MapsComponent implements OnInit {
+
+    // initial center position for the map
     lat: number = 22.6464226;
     lng: number = -103.0205916;
+    zoom: number = 10;
+    markers: Marker[];
+
 
     constructor() {
 
+        this.markers = [
+            {
+                lat: 22.6464226,
+                lng: -103.0205916,
+                label: "Jerez",
+                draggable: true
+            },
+            {
+                lat: 22.7766572,
+                lng: -102.9371446,
+                label: 'Los Haro',
+                draggable: true
+            }
+        ]
     }
 
     ngOnInit() {
+        console.log(this.markers)
     }
+
+    mapClicked($event: any) {
+        console.log($event);
+        this.markers.push({
+            lat: $event.coords.lat,
+            lng: $event.coords.lng,
+            draggable: true
+
+        })
+    }
+
+    clickedMarker(label: string, index: number) {
+        console.log(`clicked the marker: ${label || index}`)
+    }
+
+
+
+
+
+
+}
+
+
+
+interface Marker {
+    lat: number;
+    lng: number;
+    label?: string;
+    draggable: boolean;
 }
