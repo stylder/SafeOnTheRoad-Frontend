1f023861997565be118444da07938748c4e4c4d9
stylder <Santiago1>
2017-08-30 21:53:15 -0500

    first commit

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { LayoutComponent } from './layout.component';
+import { HeaderComponent, SidebarComponent } from '../shared';
+import { MapsComponent } from './maps/maps.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        NgbDropdownModule.forRoot(),
+        LayoutRoutingModule,
+        TranslateModule
+    ],
+    declarations: [
+        LayoutComponent,
+        HeaderComponent,
+        SidebarComponent,
+        MapsComponent,
+    ]
+})
+export class LayoutModule { }
